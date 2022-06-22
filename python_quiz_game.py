58e65de18f73d322e7736f590d84d098dc115c21
ugurduyar <ismailugurduyar@gmail.com>
2022-06-22 22:54:58 +0300

    percentage

--- python_quiz_game.py
+++ python_quiz_game.py
@@ -48,3 +48,4 @@ else:
 print("You have {} points!\n".format(points))
 question_count += 1
 print("Congratulations! You did {} out of {} ".format(points,question_count))
+print("You did {} percent right".format(round((points/question_count)*100)))
