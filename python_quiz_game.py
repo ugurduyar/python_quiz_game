4c7efb37d9b11dce3dae1d2491d44b84eaf2c6a5
ugurduyar <ismailugurduyar@gmail.com>
2022-06-22 20:49:30 +0300

    first question without casefold

--- python_quiz_game.py
+++ python_quiz_game.py
@@ -5,3 +5,11 @@ if want_to_play != "ok":
     quit()
 else:
     print("Let's get started!")
+points = 0
+question1 = input("What year did the world war 2 start? \nA)1939 \nB)1938\nC)1937\nD)1936\nYour answer is: ")
+if question1 == "a":
+    print("Congratulations!")
+    points += 1
+else:
+    print("Sorry, that was wrong")
+print(points)
