626ae11c47a61acd006a7cec2ed47f99726bbf31
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 12:42:56 +0300

    added .lower()

--- python_quiz_game.py
+++ python_quiz_game.py
@@ -8,7 +8,7 @@ else:
 points = 0
 question_count = 0
 question = input("What year did the world war 2 start? \nA)1939 \nB)1938\nC)1937\nD)1936\nYour answer is: ")
-if question == "A" or question == "a":
+if question.lower() == "a":
     print("Congratulations!\n")
     points += 1
 else:
@@ -16,7 +16,7 @@ else:
 print("You have {} points!\n".format(points))
 question_count += 1
 question = input("Who first discovered America? \nA)Leif Erikson \nB)Christopher Columbus\nC)Paria Peninsula\nD)William Bradford\nYour answer is: ")
-if question == "A" or question == "a":
+if question.lower() == "a":
     print("Congratulations!\n")
     points += 1
 else:
@@ -24,7 +24,7 @@ else:
 print("You have {} points!\n".format(points))
 question_count += 1
 question = input("When did the American Civil War end? \nA)1845 \nB)1855\nC)1865\nD)1875\nYour answer is: ")
-if question == "C" or question == "c":
+if question.lower() == "c":
     print("Congratulations!\n")
     points += 1
 else:
@@ -32,7 +32,7 @@ else:
 print("You have {} points!\n".format(points))
 question_count += 1
 question = input("Who invented the first car? \nA)Henry Ford \nB)Nicolas-Joseph Cugnot\nC)George B. Selden\nD)Karl Benz\nYour answer is: ")
-if question == "D" or question == "d":
+if question.lower() == "d":
     print("Congratulations!\n")
     points += 1
 else:
@@ -40,13 +40,13 @@ else:
 print("You have {} points!\n".format(points))
 question_count += 1
 question = input("NATO was formed in ... \nA)1949 \nB)1945\nC)1952\nD)1941\nYour answer is: ")
-if question == "A" or question == "a":
+if question.lower() == "a":
     print("Congratulations!\n")
     points += 1
 else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
-print("Congratulations! You did {} out of {} ".format(points,question_count))
+print("Congratulations! You did {} out of {} \n".format(points,question_count))
 print("You did {} percent right\n".format(round((points/question_count)*100)))
 input("Thanks for playing!")
