de51edbcd6d15d048c403c3cf357118b868ad2f6
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 12:45:13 +0300

    format

--- python_quiz_game.py
+++ python_quiz_game.py
@@ -5,8 +5,12 @@ if want_to_play != "ok":
     quit()
 else:
     print("Let's get started!\n")
+
+
 points = 0
 question_count = 0
+
+
 question = input("What year did the world war 2 start? \nA)1939 \nB)1938\nC)1937\nD)1936\nYour answer is: ")
 if question.lower() == "a":
     print("Congratulations!\n")
@@ -15,6 +19,8 @@ else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
+
+
 question = input("Who first discovered America? \nA)Leif Erikson \nB)Christopher Columbus\nC)Paria Peninsula\nD)William Bradford\nYour answer is: ")
 if question.lower() == "a":
     print("Congratulations!\n")
@@ -31,6 +37,8 @@ else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
+
+
 question = input("Who invented the first car? \nA)Henry Ford \nB)Nicolas-Joseph Cugnot\nC)George B. Selden\nD)Karl Benz\nYour answer is: ")
 if question.lower() == "d":
     print("Congratulations!\n")
@@ -39,6 +47,8 @@ else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
+
+
 question = input("NATO was formed in ... \nA)1949 \nB)1945\nC)1952\nD)1941\nYour answer is: ")
 if question.lower() == "a":
     print("Congratulations!\n")
@@ -47,6 +57,6 @@ else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
-print("Congratulations! You did {} out of {} \n".format(points,question_count))
+print("Test Finished! You did {} out of {} \n".format(points,question_count))
 print("You did {} percent right\n".format(round((points/question_count)*100)))
 input("Thanks for playing!")
