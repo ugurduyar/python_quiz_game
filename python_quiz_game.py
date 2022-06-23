49c1fae48bcb8ee5649ecbf0f378e0ca290b8637
ugurduyar <ismailugurduyar@gmail.com>
2022-06-23 12:32:50 +0300

    removed question numbers, unnecesarry

--- python_quiz_game.py
+++ python_quiz_game.py
@@ -7,40 +7,40 @@ else:
     print("Let's get started!\n")
 points = 0
 question_count = 0
-question1 = input("What year did the world war 2 start? \nA)1939 \nB)1938\nC)1937\nD)1936\nYour answer is: ")
-if question1 == "A" or question1 == "a":
+question = input("What year did the world war 2 start? \nA)1939 \nB)1938\nC)1937\nD)1936\nYour answer is: ")
+if question == "A" or question == "a":
     print("Congratulations!\n")
     points += 1
 else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
-question2 = input("Who first discovered America? \nA)Leif Erikson \nB)Christopher Columbus\nC)Paria Peninsula\nD)William Bradford\nYour answer is: ")
-if question2 == "A" or question2 == "a":
+question = input("Who first discovered America? \nA)Leif Erikson \nB)Christopher Columbus\nC)Paria Peninsula\nD)William Bradford\nYour answer is: ")
+if question == "A" or question == "a":
     print("Congratulations!\n")
     points += 1
 else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
-question3 = input("When did the American Civil War end? \nA)1845 \nB)1855\nC)1865\nD)1875\nYour answer is: ")
-if question3 == "C" or question3 == "c":
+question = input("When did the American Civil War end? \nA)1845 \nB)1855\nC)1865\nD)1875\nYour answer is: ")
+if question == "C" or question == "c":
     print("Congratulations!\n")
     points += 1
 else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
-question4 = input("Who invented the first car? \nA)Henry Ford \nB)Nicolas-Joseph Cugnot\nC)George B. Selden\nD)Karl Benz\nYour answer is: ")
-if question4 == "D" or question4 == "d":
+question = input("Who invented the first car? \nA)Henry Ford \nB)Nicolas-Joseph Cugnot\nC)George B. Selden\nD)Karl Benz\nYour answer is: ")
+if question == "D" or question == "d":
     print("Congratulations!\n")
     points += 1
 else:
     print("Sorry, that was wrong\n")
 print("You have {} points!\n".format(points))
 question_count += 1
-question5 = input("NATO was formed in ... \nA)1949 \nB)1945\nC)1952\nD)1941\nYour answer is: ")
-if question5 == "A" or question5 == "a":
+question = input("NATO was formed in ... \nA)1949 \nB)1945\nC)1952\nD)1941\nYour answer is: ")
+if question == "A" or question == "a":
     print("Congratulations!\n")
     points += 1
 else:
